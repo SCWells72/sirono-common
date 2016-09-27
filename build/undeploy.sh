b20413cc00d676e0648bcd2cfd9c2698bb2f3c4e
mmartinATX <mmartinATX@users.noreply.github.com>
2016-09-27 14:22:26 -0500

    Add build target to delete changes (#10)

---
+++ build/undeploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#deploy destructiveChanges.xml
+result=$((ant undeployCode -Dsf.target.username=$sfusername -Dsf.target.password=$sfpassword -Dsf.target.serverurl=$sfserverurl) 2>&1)
+ 
+echo "result: $result"
+#parse the build output, allow Warnings but fail on FAILED
+if [[ $result == *Warning* ]]; then
+    echo '**** Got a warning, probably okay - Passing ****'
+    exit 0;
+elif [[ $result == *FAILED* ]]; then
+    echo '**** Got an error - Failing ****'
+    exit 3;
+fi
+exit 0;
