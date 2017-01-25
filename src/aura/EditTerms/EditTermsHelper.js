b62e69b38e2d4845103872df1a80e6cb6a025306
jetbi_denisshpilevsky <denis.shpilevsky@jet-bi.com>
2017-01-25 21:00:03 +0300

    Changes on "Create Payment Plan", "Make a Payment" tabs and "List of Invoices" component.

--- src/aura/EditTerms/EditTermsHelper.js
+++ src/aura/EditTerms/EditTermsHelper.js
@@ -1,5 +1,8 @@
 ({
-	helperMethod: function () {
-
+	showError: function(cmp, message) {
+		console.error(message);
+		cmp.set('v.hasError', true);
+		cmp.set('v.severity', 'error');
+		cmp.set('v.message', message);
 	}
 })
--- src/aura/SingleInvoice/SingleInvoiceController.js
+++ src/aura/SingleInvoice/SingleInvoiceController.js
@@ -1,5 +1,30 @@
 ({
 	myAction: function (component, event, helper) {
 
+	},
+
+	selectInvoice: function(component, event, helper){
+			$A.util.toggleClass(component.find('selectIcon'), 'selected');
+			var activated = component.get('v.activated');
+			var balanceDue = component.get('v.invoice').balanceDue;
+			if(!activated){
+				balanceDue = - balanceDue;
+			}
+			var calculatePaymentBalance  = $A.get("e.c:calculatePaymentBalanceEvent");
+			calculatePaymentBalance.setParams({
+				"changeSum" : balanceDue,
+			});
+			calculatePaymentBalance.fire();
+			component.set('v.activated', !activated);
+	},
+
+	openInvoiceDetails : function(component, event, helper){
+		console.log('open invoice details');
+		console.log(component.get("v.tileId"));
+		$A.get("e.c:activateTileRequest").setParams({
+			"tileId" : component.get("v.tileId")
+		}).fire();
 	}
+
+
 })
