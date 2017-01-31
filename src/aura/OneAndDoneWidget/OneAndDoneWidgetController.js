429668f1b805602a4994bb0f77991f84daaf6787
Oleg Lutsevich <oleg.lutsevich@cadalys.com>
2017-01-31 02:30:01 +0200

    Add Account Summary, widgets

--- src/aura/OneAndDoneWidget/OneAndDoneWidgetController.js
+++ src/aura/OneAndDoneWidget/OneAndDoneWidgetController.js
@@ -3,5 +3,17 @@
         $A.get("e.force:navigateToURL").setParams({
 			'url' : '/payments' 
 		}).fire();
+	},
+	sendPageMakeToHeader: function(component, event, helper) {
+		console.log("PageMake");
+        var appEvent = $A.get("e.c:payNowRequest");
+        appEvent.setParams({ 'type': 'MakeAPayment' });
+        appEvent.fire();
+	},
+	sendPageCreateToHeader: function(component, event, helper) {
+		console.log("PageCreate");
+        var appEvent = $A.get("e.c:payNowRequest");
+        appEvent.setParams({ 'type': 'CreatePaymentPlan' });
+        appEvent.fire();
 	}
 })
