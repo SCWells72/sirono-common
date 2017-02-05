229df25b1889790750ffe4a45a4b8a8305e1dde7
Vladislav Lipen <vladislav.lipen@gmail.com>
2017-02-05 18:40:10 +0300

    fixed styles

--- src/aura/EstimateDetails/EstimateDetailsController.js
+++ src/aura/EstimateDetails/EstimateDetailsController.js
@@ -5,5 +5,11 @@
 			component.set("v.activeEstimate",tileId);
 			component.set("v.estimate",component.get("v.listOfEstimates")[tileId]);
 		}
-	}
+	},
+
+	sendEstimateNowToHeader: function(component, event, helper) {
+        var appEvent = $A.get("e.c:payNowRequest");
+        appEvent.setParams({ "invoiceId" : component.get('v.estimate.singleEncounter.Id'), 'type': 'MakeAPayment', 'isEstimate': true });
+        appEvent.fire();
+	},
 })
