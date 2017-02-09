91c3efb94cc47849638a0b4468ae6626b260f8e6
Vladislav Lipen <vladislav.lipen@gmail.com>
2017-02-09 08:45:39 +0300

    Fixed estimates

--- src/aura/Estimates/EstimatesController.js
+++ src/aura/Estimates/EstimatesController.js
@@ -16,4 +16,48 @@
             $A.util.removeClass(scroll,"alert");
         }
 	},
+	activateSelect : function(component, event, helper) {
+		var blockSelect = component.find('blockSelect'); 
+		$A.util.toggleClass(blockSelect, 'showSelect');   
+	},
+	patientsVisibility : function(component, Event, helper) {
+		var blockSelect = component.find('patientList');
+		$A.util.toggleClass(blockSelect, 'slds-is-open');
+	},
+	patientSelect : function(component, event, helper) {
+		$A.util.toggleClass(event.currentTarget, 'slds-is-selected');
+		var patientId = event.currentTarget.dataset.patientId;
+		var patientSetOld = component.get('v.patientSet');
+		var patientLabel = '';
+		var selectCounter = 0;
+		for(var i = 0; i < patientSetOld.length; i++) {		
+			if(patientSetOld[i].id == patientId) {
+				patientSetOld[i].isSelected = $A.util.hasClass(event.currentTarget, 'slds-is-selected');
+			}	
+			if(patientSetOld[i].isSelected) {
+				selectCounter += 1;
+				patientLabel += patientSetOld[i].name + ", ";
+			}			
+		}
+		patientLabel = patientLabel.substring(0, patientLabel.length - 2);
+		if(selectCounter != patientSetOld.length) {
+			component.set('v.patientLabel', patientLabel);
+		} else {
+			component.set('v.patientLabel', 'All Patients');
+		}
+
+		helper.getAllEstimates(component);
+	},
+
+	filterEstimates : function (component, event, helper) {
+		console.log(' event.target.id',  event.target.id);
+		var filterValue = event.target.id == 'allEstimates' ? 'All' : 
+				event.target.id == 'unpaidEstimates' ? 'Unpaid' : 'Paid';
+		console.log('filterValue', filterValue);
+		if(component.get('v.groupFilter') != filterValue){
+			component.set('v.patientSet', undefined);
+            component.set('v.groupFilter', filterValue);
+            helper.getAllEstimates(component);	
+        } 
+	}
 })
--- src/aura/Estimates/EstimatesHelper.js
+++ src/aura/Estimates/EstimatesHelper.js
@@ -1,17 +1,40 @@
 ({
 	getAllEstimates : function(component) {
 		var action = component.get("c.getAllEstimates");
+		var additionalFilter = '';
+		
+		var selectedPatients = component.get('v.patientSet');
+		if(selectedPatients != null) {
+			for(var i = 0; i < selectedPatients.length; i++) {
+				if(selectedPatients[i].isSelected == true) {
+					additionalFilter += "'" + selectedPatients[i].id + "',";
+				}
+			}
+		}
 		
 		action.setParams({
 			'groupFilter' : component.get("v.groupFilter"),
-			'additionalFilter' : component.get("v.additionalFilter")
+			'additionalFilter' : additionalFilter
 		});
 		
 		action.setCallback(this, function(response) {
 			var state = response.getState();
 			if (state === "SUCCESS") {
 				var listOfEstimates = response.getReturnValue();
-				console.log('listOfEstimates ',listOfEstimates);
+				console.log('listOfEstimates', listOfEstimates);
+
+				if (component.get("v.groupFilter") == 'All') {
+					if (listOfEstimates.length > 0) {
+						$A.util.removeClass(component.find('main-body'), 'display-none');
+						$A.util.removeClass(component.find('estimate_details'), 'display-none');
+					} else {
+						$A.util.removeClass(component.find('empty-estimates'), 'display-none');
+					}
+				}
+
+				if(additionalFilter == '') {
+					this.buildPatientList(component, listOfEstimates, selectedPatients);
+				}
 				this.createTiles(component,listOfEstimates);
 			} else if (state === "ERROR") {
 				var errors = response.getError();
@@ -28,16 +51,40 @@
         $A.enqueueAction(action);
 	},
 
+	buildPatientList : function(component, listOfEstimates, selectedPatients) {
+		//init patient list
+		var patientList = [];
+		var patientSet  = new Set();
+		for (var i = 0; i < listOfEstimates.length; i++) {
+			if(!patientSet.has(listOfEstimates[i].singleEncounter.Patient__r.Id)) {
+				var patientMRN = '';
+				if(listOfEstimates[i].singleEncounter.Patient__r.Medical_Record_Number__c != undefined && listOfEstimates[i].singleEncounter.Patient__r.Medical_Record_Number__c != null) {
+					patientMRN = ' (MRN: ' + listOfEstimates[i].singleEncounter.Patient__r.Medical_Record_Number__c + ')';
+				}
+				patientList.push({
+							id : listOfEstimates[i].singleEncounter.Patient__r.Id,
+						  name : listOfEstimates[i].singleEncounter.Patient__r.Name,
+					isSelected : true,
+					       MRN : patientMRN
+				});
+				patientSet.add(listOfEstimates[i].singleEncounter.Patient__r.Id);
+			}
+		}
+		component.set('v.patientSet', patientList);
+		component.set('v.patientLabel', 'All Patients');
+	},
+
 	createTiles : function(component,listOfEstimates) {
 		
-		component.set('v.invoices',[]);
+		component.set('v.estivates',[]);
 
 		for (var i = 0; i < listOfEstimates.length; i++) {
 			$A.createComponent(
 				"c:Estimate",
 				{
 					"estimate" : listOfEstimates[i],
-					"tileId" : i
+					"tileId" : i,
+					'isOnPaymentsPage' : false
 				},
 				function(estivate, status, errorMessage) {
 					if (status === "SUCCESS") {
