efdf99a7466d809fcfa01a3dd53e999a03547c1d
Gordon Bockus <gordon.bockus@wagion.io>
2017-06-10 01:44:44 -0500

    Lightning updates due to refactor of some property names [Fixes #146655947]

--- src/aura/SironoHeader/SironoHeaderHelper.js
+++ src/aura/SironoHeader/SironoHeaderHelper.js
@@ -1,25 +1,26 @@
 ({
-	getAllHeaderInfo : function(component) {
-		var action = component.get("c.getAllHeaderInfo");
-		var formatter = new Intl.NumberFormat('en-US', {
-		  style: 'currency',
-		  currency: 'USD',
-		  minimumFractionDigits: 0,
-		});
-        action.setCallback(this, function(response) {
-        	var state = response.getState();
-	    	if (state === 'SUCCESS') {
-	    		var guarantorWrapper = response.getReturnValue();
-	            component.set('v.guarantorWrapper', guarantorWrapper);
-	            component.set('v.invoiceValue', formatter.format(Math.floor(guarantorWrapper.paymentPlan.Installment_Amount__c)));
-        		component.set('v.invoiceValuePart', (guarantorWrapper.paymentPlan.Installment_Amount__c % 1).toFixed(2).toString().substring(2));
-				var isWarning = guarantorWrapper.contact.Guarantor_Status__c == 'Overdue';
-        		component.set('v.warningMessage', isWarning);
-				var isError = guarantorWrapper.contact.Guarantor_Status__c == 'Delinquent';
-        		component.set('v.errorMessage', isError);
-  
-	    	}
-        });
-        $A.enqueueAction(action);
-	}
+  getAllHeaderInfo: function (component) {
+    var action = component.get("c.getAllHeaderInfo");
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+    });
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state === 'SUCCESS') {
+        var guarantorWrapper = response.getReturnValue();
+        console.log({guarantorWrapper: guarantorWrapper}, 'Reponse from getAllHeaderInfo.');
+        component.set('v.guarantorWrapper', guarantorWrapper);
+        component.set('v.invoiceValue', formatter.format(Math.floor(guarantorWrapper.paymentPlan.Installment_Amount__c)));
+        component.set('v.invoiceValuePart', (guarantorWrapper.paymentPlan.Installment_Amount__c % 1).toFixed(2).toString().substring(2));
+        var isWarning = guarantorWrapper.contact.Guarantor_Status__c == 'Overdue';
+        component.set('v.warningMessage', isWarning);
+        var isError = guarantorWrapper.contact.Guarantor_Status__c == 'Delinquent';
+        component.set('v.errorMessage', isError);
+
+      }
+    });
+    $A.enqueueAction(action);
+  }
 })
