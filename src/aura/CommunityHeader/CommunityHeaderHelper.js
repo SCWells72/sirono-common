a828876efd89f4adb525c140f165e39bafa2858e
Gordon Bockus <gordon.bockus@wagion.io>
2017-08-10 22:59:16 -0500

    Update the image used in the community header to be a static resource specified in the Sirono Portal Settings custom setting [Fixes #150097079] Update the css for the financial aid button so that when there are two buttons is displays correctly.  Also rename custom setting to Financial Assistance Document [Fixes #148621713] Delete the TWS static resource.

--- src/aura/CommunityHeader/CommunityHeaderHelper.js
+++ src/aura/CommunityHeader/CommunityHeaderHelper.js
@@ -3,7 +3,11 @@
  */
 
 ({
-    getUserInfo: function (component) {
+    /**
+     * Load the user information and populate the component attribute.
+     * @param {Component} component
+     */
+    getUserInfo: function(component) {
         var action = component.get("c.getUserInfo");
         action.setCallback(this, function (response) {
             var state = response.getState();
@@ -13,5 +17,30 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    /**
+     * Load the settings in order to get the org specific header image static resource name.
+     *
+     * @param {Component} component
+     */
+    getSironoSettings: function(component) {
+        var settingsService = component.find('settingsService');
+        settingsService.getSettings(function(err, settings) {
+            if (err) {
+                //TODO: we need to figure out a general error handler
+                console.log(err);
+                return;
+            }
+
+            if (settings.headerImageName && settings.headerImageName !== '') {
+                component.set('v.headerLogoUrl', $A.get('$Resource.' + settings.headerImageName ));
+            } else {
+                component.set('v.headerLogoUrl', $A.get('$Resource.SironoData') + '/assets/images/health-system-logo.png');
+            }
+            // Remove the hide class so that the logo will show up in the UI. It is initially hidden to avoid shouding the
+            // alt message initially.
+            $A.util.removeClass(component.find('headerLogo'), 'slds-hide');
+        });
     }
 })
